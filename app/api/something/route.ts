a097817bf932fea005ea7a4dfb81051d353be167
nkalupahana <avishkank@gmail.com>
2023-09-07 11:41:58 -0500

    Change formatting

--- app/api/something/route.ts
+++ app/api/something/route.ts
@@ -1,16 +1,16 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export const GET = (request: NextRequest) =>  {
-  return NextResponse.json(
-    {
-      body: request.body,
-      path: request.nextUrl.pathname,
-      query: request.nextUrl.search,
-      cookies: request.cookies.getAll(),
-    },
-    {
-      status: 200,
-    },
-  );
+export const GET = (request: NextRequest) => {
+    return NextResponse.json(
+        {
+            body: request.body,
+            path: request.nextUrl.pathname,
+            query: request.nextUrl.search,
+            cookies: request.cookies.getAll(),
+        },
+        {
+            status: 200,
+        },
+    );
 }
--- models/User.ts
+++ models/User.ts
@@ -1,24 +1,24 @@
 import mongoose from "mongoose"
 
 export interface User extends mongoose.Document {
-  name: string
-  email: string
+    name: string
+    email: string
 }
 
 /* PetSchema will correspond to a collection in your MongoDB database. */
 const UserSchema = new mongoose.Schema<User>({
-  name: {
-    /* The name of this pet */
+    name: {
+        /* The name of this pet */
 
-    type: String,
-    required: [true, "Please provide a name for this user."],
-  },
-  email: {
-    /* The owner of this pet */
+        type: String,
+        required: [true, "Please provide a name for this user."],
+    },
+    email: {
+        /* The owner of this pet */
 
-    type: String,
-    required: [true, "Please provide an email for this user."],
-  }
+        type: String,
+        required: [true, "Please provide an email for this user."],
+    }
 })
 
 export default mongoose.models.User || mongoose.model<User>("User", UserSchema)
