e3e5766935d988171b042877f93a5da34785d208
michaelperl <mperl0914@gmail.com>
2023-09-26 10:22:48 -0500

    nextUI-configured

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,17 @@
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
-import './globals.css'
+import "./globals.css";
+import { NextUIProvider } from "@nextui-org/react";
 
-export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
-    return (
-        <SessionProvider session={session}>
-            <Component {...pageProps} />
-        </SessionProvider>
-    );
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <NextUIProvider>
+        <Component {...pageProps} />
+      </NextUIProvider>
+    </SessionProvider>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,55 +3,82 @@ import useSWR from "swr";
 import { fetcher } from "@/lib/swr";
 import { MusicFile } from "@/models/MusicFile";
 import { useRef, useState } from "react";
+import { Button } from "@nextui-org/button";
 
 export default function Index() {
-    const { data: session, status } = useSession();
-    const { data: musicList } = useSWR("/api/music/list", fetcher);
-    const audio = useRef<HTMLAudioElement>(null);
+  const { data: session, status } = useSession();
+  const { data: musicList } = useSWR("/api/music/list", fetcher);
+  const audio = useRef<HTMLAudioElement>(null);
 
-    return (
+  return (
+    <>
+      {status === "loading" && <p>Hang on there...</p>}
+      {status === "unauthenticated" && (
         <>
-            { status === "loading" && <p>Hang on there...</p> }
-            { status === "unauthenticated" && <>
-                <p>Not signed in.</p> 
-                <button className="btn btn-primary" onClick={() => signIn("google")}>Sign in with Google</button>
-            </> }
-            { status === "authenticated" && <>
-                <p>Signed in as { session?.user?.email }</p>
-                <p>Is Admin: { String(session?.user?.isAdmin) }</p>
-                <p>Subscribed: { String(session?.user?.subscribed) }</p>
-                <button className="btn btn-primary" onClick={() => signOut()}>Sign out</button>
-            </> }
-            <table>
-                <thead>
-                    <tr>
-                        <td>Song Name</td>
-                        <td>Tempo</td>
-                        <td>Play</td>
-                        <td>Download</td>
-                    </tr>
-                </thead>
-                { musicList?.map((music: MusicFile) => <tr key={music._id}>
-                    <td>{ music.name }</td>
-                    <td>{ music.tempo }</td>
-                    <td onClick={() => {
-                        audio.current!.src = `/api/music/play?id=${music._id}`; 
-                        audio.current!.play();
-                    }}>Play</td>
-                    <td onClick={() => {
-                        window.open(`/api/music/download?id=${music._id}`)
-                    }}>Download</td>
-                </tr>) }
-            </table>
-            <audio ref={audio}></audio>
-            <br /><br />
-            <p>Upload Music</p>
-            <form method="POST" action="/api/music/upload" encType="multipart/form-data">
-                <input type="text" name="songname" placeholder="Song Name" required /><br />
-                <input type="number" name="tempo" placeholder="Tempo" required /><br />
-                <input type="file" name="file" accept="audio/mpeg" required /><br />
-                <input type="submit" value="Upload" />
-            </form>
+          <p>Not signed in.</p>
+          <button className="btn btn-primary" onClick={() => signIn("google")}>
+            Sign in with Google
+          </button>
         </>
-    );
+      )}
+      {status === "authenticated" && (
+        <>
+          <p>Signed in as {session?.user?.email}</p>
+          <p>Is Admin: {String(session?.user?.isAdmin)}</p>
+          <p>Subscribed: {String(session?.user?.subscribed)}</p>
+          <Button color="primary" onClick={() => signOut()}>
+            Sign out
+          </Button>
+        </>
+      )}
+      <table>
+        <thead>
+          <tr>
+            <td>Song Name</td>
+            <td>Tempo</td>
+            <td>Play</td>
+            <td>Download</td>
+          </tr>
+        </thead>
+        {musicList?.map((music: MusicFile) => (
+          <tr key={music._id}>
+            <td>{music.name}</td>
+            <td>{music.tempo}</td>
+            <td
+              onClick={() => {
+                audio.current!.src = `/api/music/play?id=${music._id}`;
+                audio.current!.play();
+              }}
+            >
+              Play
+            </td>
+            <td
+              onClick={() => {
+                window.open(`/api/music/download?id=${music._id}`);
+              }}
+            >
+              Download
+            </td>
+          </tr>
+        ))}
+      </table>
+      <audio ref={audio}></audio>
+      <br />
+      <br />
+      <p>Upload Music</p>
+      <form
+        method="POST"
+        action="/api/music/upload"
+        encType="multipart/form-data"
+      >
+        <input type="text" name="songname" placeholder="Song Name" required />
+        <br />
+        <input type="number" name="tempo" placeholder="Tempo" required />
+        <br />
+        <input type="file" name="file" accept="audio/mpeg" required />
+        <br />
+        <input type="submit" value="Upload" />
+      </form>
+    </>
+  );
 }
