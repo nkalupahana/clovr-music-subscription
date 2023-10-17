db17d711d0421310ddf77587b27fb12a0dfde905
nkalupahana <avishkank@gmail.com>
2023-10-16 20:29:07 -0500

    Models updated for new MusicFile

--- models/MusicFile.ts
+++ models/MusicFile.ts
@@ -2,9 +2,12 @@ import mongoose from "mongoose"
 
 export interface MusicFile extends mongoose.Document {
     name: string
-    tempo: number,
-    key: string,
-    uploadTime: Date,
+    artist: string
+    tempo: number
+    songKey: string
+    albumArtKey: string
+    releaseDate: string
+    uploadTime: Date
 }
 
 const MusicFileSchema = new mongoose.Schema<MusicFile>({
@@ -12,18 +15,30 @@ const MusicFileSchema = new mongoose.Schema<MusicFile>({
         type: String,
         required: [true, "Please provide a name for this song."],
     },
+    artist: {
+        type: String,
+        required: [true, "Please provide an artist for this song."],
+    },
     tempo: {
         type: Number,
         required: [true, "Please provide an tempo for this song."],
     },
-    key: {
+    songKey: {
         type: String,
         required: [true, "Please provide an R2 key for this song."],
     },
+    albumArtKey: {
+        type: String,
+        required: [true, "Please provide an R2 key for the album art for this song."],
+    },
+    releaseDate: {
+        type: String,
+        required: [true, "Please provide a release date for this song."],
+    },
     uploadTime: {
         type: Date,
-        default: Date.now,
-    },
+        default: Date.now
+    }
 })
 
 export default mongoose.models.MusicFile || mongoose.model<MusicFile>("MusicFile", MusicFileSchema)
--- pages/api/music/upload.ts
+++ pages/api/music/upload.ts
@@ -3,6 +3,8 @@ import { r2 } from "@/lib/r2";
 import MusicFile from "@/models/MusicFile";
 import formidable from "formidable";
 import fs from "fs";
+import Sharp from "sharp";
+import { DateTime } from "luxon";
 
 import type { NextApiRequest, NextApiResponse } from "next"
 
@@ -24,27 +26,58 @@ export default async function handler(
             if (!fields.songname || fields.songname.length !== 1 || !fields.songname[0] || typeof fields.songname[0] !== "string") {
                 return res.redirect("/?message=Invalid song name!")
             }
-
-            if (!fields.tempo || fields.tempo.length !== 1 || !fields.tempo[0] || typeof fields.tempo[0] !== "string" || fields.tempo[0] !== String(Number(fields.tempo[0]))) {
-                return res.redirect("/?message=Invalid song name!")
+            
+            if (!fields.duration || fields.duration.length !== 1 || !fields.duration[0] || typeof fields.duration[0] !== "string" || fields.duration[0] !== String(Number(fields.duration[0]))) {
+                return res.redirect("/?message=Invalid tempo!")
             }
 
-            if (!files.file || files.file.length !== 1 || !files.file[0] || typeof files.file[0] !== "object" || files.file[0].mimetype !== "audio/mpeg") {
-                return res.redirect("/?message=Invalid file!")
+            if (!fields.artist || fields.artist.length !== 1 || !fields.artist[0] || typeof fields.artist[0] !== "string" || fields.artist[0].length > 1000) {
+                return res.redirect("/?message=Invalid artist name!")
             }
 
+            if (!fields.releaseDate || fields.releaseDate.length !== 1 || !fields.releaseDate[0] || typeof fields.releaseDate[0] !== "string") {
+                return res.redirect("/?message=Invalid release date!")
+            }
+
+            const releaseDate = DateTime.fromISO(fields.releaseDate[0]);
+            if (!releaseDate.isValid) {
+                return res.redirect("/?message=Invalid release date!")
+            }
+
+            if (!files.musicFile || files.musicFile.length !== 1 || !files.musicFile[0] || typeof files.musicFile[0] !== "object" || files.musicFile[0].mimetype !== "audio/wav") {
+                return res.redirect("/?message=Invalid music file!")
+            }
+
+            if (!files.albumArtFile || files.albumArtFile.length !== 1 || !files.albumArtFile[0] || typeof files.albumArtFile[0] !== "object" || !files.albumArtFile[0].mimetype?.startsWith("image/")) {
+                return res.redirect("/?message=Invalid album art file!")
+            }
+
+            // Upload song
             await r2.putObject({ 
                 Bucket: process.env.R2_BUCKET ?? "", 
-                Key: files.file[0].newFilename + ".mp3", 
-                Body: fs.readFileSync(files.file[0].filepath),
-                ContentDisposition: "attachment; filename=" + fields.songname[0] + ".mp3",
-                ContentType: "audio/mpeg"
+                Key: files.musicFile[0].newFilename + ".wav", 
+                Body: fs.readFileSync(files.musicFile[0].filepath),
+                ContentDisposition: "attachment; filename=" + fields.songname[0] + ".wav",
+                ContentType: "audio/wav"
             }).promise();
 
+            // Standardize and upload album art
+            const albumArt = await Sharp(files.albumArtFile[0].filepath).rotate().webp().toBuffer();
+            await r2.putObject({
+                Bucket: process.env.R2_BUCKET ?? "", 
+                Key: files.albumArtFile[0].newFilename + ".webp",
+                Body: albumArt
+            }).promise();
+
+            // Create file record
             await MusicFile.create({
                 name: fields.songname[0],
-                tempo: Number(fields.tempo[0]),
-                key: files.file[0].newFilename + ".mp3",
+                tempo: 120 + Math.round(Math.random() * 60), // todo: implement tempo detection
+                duration: Number(fields.duration[0]), // todo: implement duration detection
+                songKey: files.musicFile[0].newFilename + ".wav",
+                albumArtKey: files.albumArtFile[0].newFilename + ".webp",
+                artist: fields.artist[0],
+                releaseDate: releaseDate.toISODate()
             });
         } catch (e) {
             console.log(e);
