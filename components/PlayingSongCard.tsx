eac17b34ff91a16dfdad58f7d48a61afb6e3a221
michaelperl0914 <62848791+michaelperl0914@users.noreply.github.com>
2023-11-07 22:28:41 -0600

    Add animation to PlayingSongCard component and (#5)

--- components/PlayingSongCard.tsx
+++ components/PlayingSongCard.tsx
@@ -56,64 +56,66 @@ export const PlayingSongCard = () => {
   };
 
   return (
-    <Card
-      isBlurred
-      className="border-none bg-background/60 dark:bg-primary "
-      shadow="sm"
-    >
+    <>
       {playingSong && (
-        <CardBody>
-          <div className="grid grid-cols-6 md:grid-cols-12 gap-6 md:gap-4 items-center justify-center">
-            <div className="relative col-span-6 md:col-span-4">
-              <Image
-                alt="Album cover"
-                className="object-cover"
-                height={200}
-                shadow="md"
-                src={`${process.env.NEXT_PUBLIC_CDN_URL}${playingSong?.albumArtKey}`}
-                width={200}
-              />
-            </div>
+        <Card
+          isBlurred
+          className="border-none bg-background/60 dark:bg-primary drop-in"
+          shadow="sm"
+        >
+          <CardBody>
+            <div className="grid grid-cols-6 md:grid-cols-12 gap-6 md:gap-4 items-center justify-center ">
+              <div className="relative col-span-6 md:col-span-4">
+                <Image
+                  alt="Album cover"
+                  className="object-cover"
+                  height={200}
+                  shadow="md"
+                  src={`${process.env.NEXT_PUBLIC_CDN_URL}${playingSong?.albumArtKey}`}
+                  width={200}
+                />
+              </div>
 
-            <div className="flex flex-col col-span-6 md:col-span-8">
-              <div className="flex justify-between items-center">
-                <div className="flex flex-col gap-0">
-                  <h1 className="text-2xl font-medium mt-2">
-                    {playingSong?.name}
-                  </h1>
-                  <h2 className="text-default-900/60 text-small">
-                    {playingSong?.artist}
-                  </h2>
+              <div className="flex flex-col col-span-6 md:col-span-8">
+                <div className="flex justify-between items-center">
+                  <div className="flex flex-col gap-0">
+                    <h1 className="text-2xl font-medium mt-2">
+                      {playingSong?.name}
+                    </h1>
+                    <h2 className="text-default-900/60 text-small">
+                      {playingSong?.artist}
+                    </h2>
+                  </div>
+                  <SongHeart song={playingSong} iconSize={32} />
                 </div>
-                <SongHeart song={playingSong} iconSize={32} />
-              </div>
 
-              <div className="flex flex-col mt-3 gap-1 ">
-                <SongProgressBar />
-              </div>
+                <div className="flex flex-col mt-3 gap-1 ">
+                  <SongProgressBar />
+                </div>
 
-              <div className="flex w-full items-center justify-center gap-4">
-                <BiSkipPrevious
-                  className="text-foreground hover:text-foreground/50 transition-all ease-in-out duration-200 cursor-pointer"
-                  size={42}
-                  onClick={handleReplay}
-                />
-                <ToggleAudioButton iconSize={28} />
-                <BiSkipNext
-                  className="text-foreground hover:text-foreground/50 transition-all ease-in-out duration-200 cursor-pointer"
-                  size={42}
-                  onClick={handleNext}
-                />
-                <BsShuffle
-                  className="text-foreground hover:text-foreground/50 transition-all ease-in-out duration-200 cursor-pointer"
-                  size={28}
-                  onClick={handleShuffle}
-                />
+                <div className="flex w-full items-center justify-center gap-4">
+                  <BiSkipPrevious
+                    className="text-foreground hover:text-foreground/50 transition-all ease-in-out duration-200 cursor-pointer"
+                    size={42}
+                    onClick={handleReplay}
+                  />
+                  <ToggleAudioButton iconSize={28} />
+                  <BiSkipNext
+                    className="text-foreground hover:text-foreground/50 transition-all ease-in-out duration-200 cursor-pointer"
+                    size={42}
+                    onClick={handleNext}
+                  />
+                  <BsShuffle
+                    className="text-foreground hover:text-foreground/50 transition-all ease-in-out duration-200 cursor-pointer"
+                    size={28}
+                    onClick={handleShuffle}
+                  />
+                </div>
               </div>
             </div>
-          </div>
-        </CardBody>
+          </CardBody>
+        </Card>
       )}
-    </Card>
+    </>
   );
 };
--- components/SongProgressBar.tsx
+++ components/SongProgressBar.tsx
@@ -2,15 +2,18 @@ import React, { useRef, useContext, useEffect, useState } from "react";
 import { MusicContext } from "@/context/MusicContext";
 
 const SongProgressBar: React.FC = () => {
+  const progressContainerRef = useRef<HTMLDivElement>(null);
   const progressRef = useRef<HTMLDivElement>(null);
   const context = useContext(MusicContext);
   const [songProgress, setSongProgress] = useState(0);
+  const circleRef = useRef<HTMLDivElement>(null);
+  const [isDragging, setIsDragging] = useState(false);
 
   useEffect(() => {
     const audioElement = context?.audio.current;
 
     const handleTimeUpdate = () => {
-      if (audioElement) {
+      if (audioElement && !isDragging) {
         const { currentTime, duration } = audioElement;
         const progressPercent = (currentTime / duration) * 100;
         setSongProgress(progressPercent);
@@ -22,7 +25,7 @@ const SongProgressBar: React.FC = () => {
     return () => {
       audioElement?.removeEventListener("timeupdate", handleTimeUpdate);
     };
-  }, [context, setSongProgress]);
+  }, [context, setSongProgress, isDragging]);
 
   const formatSeconds = (seconds: number) => {
     const minutes = Math.floor(seconds / 60);
@@ -42,21 +45,92 @@ const SongProgressBar: React.FC = () => {
     }
   };
 
+  // Add mouse down event to the circle
+  const handleMouseDown = (e: React.MouseEvent<HTMLDivElement>) => {
+    setIsDragging(true);
+    e.stopPropagation(); // Prevent other elements from triggering
+  };
+
+  // Add mouse move event to the window to allow dragging outside the bar
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      if (isDragging && progressContainerRef.current) {
+        const rect = progressContainerRef.current.getBoundingClientRect();
+        const x = e.clientX - rect.left; // Get mouse position relative to progress bar
+        const width = rect.width;
+        const progressPercent = Math.max(0, Math.min(1, x / width)) * 100;
+        setSongProgress(progressPercent);
+      }
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, [isDragging]);
+
+  // Add mouse up event to the window to release the drag
+  useEffect(() => {
+    const handleMouseUp = () => {
+      if (isDragging) {
+        setIsDragging(false);
+        if (context?.audio.current) {
+          const newTime =
+            (context.audio.current.duration || 0) * (songProgress / 100);
+          context.audio.current.currentTime = newTime; // Set the audio element's current time
+        }
+      }
+    };
+
+    window.addEventListener("mouseup", handleMouseUp);
+    return () => {
+      window.removeEventListener("mouseup", handleMouseUp);
+    };
+  }, [isDragging, songProgress, context]);
+
   return (
-    <div className="progress-container">
+    <div className="progress-container" ref={progressContainerRef}>
       <div
         ref={progressRef}
         onClick={handleProgressClick}
-        className="relative w-full h-2 rounded-full bg-default-100/50 overflow-hidden group"
+        className="relative w-full h-2 bg-default-100/50  group"
       >
         <div
-          className="absolute top-0 left-0 h-full bg-gray-400 transition-all duration-800 ease-in-out group-hover:bg-gradient-to-r group-hover:from-yellow-500 group-hover:to-red-500"
+          className={`absolute top-0 left-0 h-full bg-gray-400 ${
+            isDragging ? "bg-gradient-to-r from-yellow-500 to-red-500" : ""
+          } `}
           style={{ width: `${songProgress}%` }}
         ></div>
+        <div
+          ref={circleRef}
+          onMouseDown={handleMouseDown}
+          className="absolute"
+          style={{
+            left: `${songProgress}%`,
+            top: "50%",
+            transform: "translate(-50%, -50%)",
+            zIndex: 10,
+            cursor: "pointer", // Indicates the element is draggable
+          }}
+        >
+          <div
+            className="w-5 h-5 bg-white rounded-full shadow"
+            style={{
+              position: "absolute",
+              top: "50%",
+              transform: "translateY(-50%)",
+            }}
+          ></div>
+
+        </div>
       </div>
-      <div className="flex justify-between">
+      <div className="flex justify-between mt-2">
         <p className="text-small">
-          {formatSeconds(context?.audio.current?.currentTime || 0)}
+          {isDragging
+            ? formatSeconds(
+                (context?.audio.current?.duration || 0) * (songProgress / 100)
+              )
+            : formatSeconds(context?.audio.current?.currentTime || 0)}
         </p>
         <p className="text-small text-foreground/50">
           {formatSeconds(context?.getSongDuration() || 0)}
