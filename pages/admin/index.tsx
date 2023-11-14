6d6ff74b53574f01271f4575fa4f287305969b35
nkalupahana <avishkank@gmail.com>
2023-11-13 23:27:41 -0600

    Try removing everything

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,24 +1,7 @@
-import PageHeader from "@/components/PageHeader";
-import { Tabs, Tab } from "@nextui-org/react";
-import { AdminMusicDashboard } from "@/components/AdminMusicDashboard";
-import { AdminUserDashboard } from "@/components/AdminUserDashboard";
 
 const Admin = () => {
   return (
     <div className="flex flex-col items-center justify-center  w-full py-2">
-      <PageHeader>Admin Dashboard</PageHeader>
-      <Tabs
-        aria-label="Options"
-        color="primary"
-        className="w-full items-center  justify-center mt-4"
-      >
-        <Tab key="music" title="Music" className="w-full text-xl">
-          <AdminMusicDashboard />
-        </Tab>
-        <Tab key="users" title="Users" className="w-full text-xl">
-          <AdminUserDashboard />
-        </Tab>
-      </Tabs>
     </div>
   );
 };
