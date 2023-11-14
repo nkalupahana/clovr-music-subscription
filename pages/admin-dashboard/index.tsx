74ada573f60afcb1035ed14d5cb866f0adc01e66
nkalupahana <avishkank@gmail.com>
2023-11-13 23:33:17 -0600

    restore page

--- pages/admin-dashboard/index.tsx
+++ pages/admin-dashboard/index.tsx
@@ -1,23 +1,24 @@
 import PageHeader from "@/components/PageHeader";
+import { Tabs, Tab } from "@nextui-org/react";
 import { AdminUserDashboard } from "@/components/AdminUserDashboard";
 
-const Admin = () => {
+const AdminDashboard = () => {
   return (
     <div className="flex flex-col items-center justify-center  w-full py-2">
       <PageHeader>Admin Dashboard</PageHeader>
-      <div
+      <Tabs
         aria-label="Options"
         color="primary"
         className="w-full items-center  justify-center mt-4"
       >
-        <div key="music" title="Music" className="w-full text-xl">
-        </div>
-        <div key="users" title="Users" className="w-full text-xl">
+        <Tab key="music" title="Music" className="w-full text-xl">
+        </Tab>
+        <Tab key="users" title="Users" className="w-full text-xl">
           <AdminUserDashboard />
-        </div>
-      </div>
+        </Tab>
+      </Tabs>
     </div>
   );
 };
 
-export default Admin;
+export default AdminDashboard;
