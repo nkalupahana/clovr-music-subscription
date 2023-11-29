79c27b6275e3ca006085f08430e71624a00c7137
michaelperl <mperl0914@gmail.com>
2023-11-29 14:15:21 -0600

    Fix landing page responsiveness

--- components/PricingCard.tsx
+++ components/PricingCard.tsx
@@ -15,11 +15,11 @@ export const PricingCard = ({
         <div className="flex flex-col items-center justify-center">
           <Image
             alt="Album cover"
-            className="object-cover w-full h-48"
+            className="object-cover"
             shadow="md"
             src={image}
           />
-          <h1 className="text-3xl font-bold text-center mt-8">{name}</h1>
+          <h1 className="text-2xl md:text-3xl font-bold text-center mt-8">{name}</h1>
           <h2 className="text-xl text-gray-400 text-center mt-8">{price}</h2>
         </div>
       </CardBody>
