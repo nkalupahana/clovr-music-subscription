c4abe60531933e8d4a50c1222254a3dcff9864cb
michaelperl <mperl0914@gmail.com>
2023-12-04 18:36:16 -0600

    small change to pricing card

--- components/PricingCard.tsx
+++ components/PricingCard.tsx
@@ -49,57 +49,60 @@ export const PricingCard = ({
           <h2 className="text-lg text-gray-400 text-center mt-8">
             ${(monthlyPrice / numChannels).toFixed(2)} / Channel
           </h2>
-          <h3 className="text-lg text-gray-400 text-center mt-8 font-bold">
-            Enter Channel Links to Subscribe!
-          </h3>
+
           {status === "authenticated" && !session?.user.subscribed && (
-            <div className="flex flex-col items-center justify-center mt-8 gap-4">
-              {Array.from({ length: numChannels }, (_, index) => {
-                return (
-                  <div
-                    className="flex flex-row items-center justify-center space-y-4"
-                    key={index}
-                  >
-                    <Input
-                      type="url"
-                      placeholder="YouTubeChannel.com"
-                      labelPlacement="outside"
-                      value={channelUrl[index]}
-                      onChange={(e) => {
-                        const newChannelUrl = [...channelUrl];
-                        newChannelUrl[index] = e.target.value;
-                        setChannelUrl(newChannelUrl);
-                      }}
-                      startContent={
-                        <div className="pointer-events-none flex items-center">
-                          <span className="text-default-400 text-medium">
-                            https://
-                          </span>
-                        </div>
-                      }
-                    />
-                  </div>
-                );
-              })}
-              <button
-                onClick={() => {
-                  if (
+            <>
+              <h3 className="text-lg text-gray-400 text-center mt-8 font-bold">
+                Enter Channel Links to Subscribe!
+              </h3>
+              <div className="flex flex-col items-center justify-center mt-8 gap-4">
+                {Array.from({ length: numChannels }, (_, index) => {
+                  return (
+                    <div
+                      className="flex flex-row items-center justify-center space-y-4"
+                      key={index}
+                    >
+                      <Input
+                        type="url"
+                        placeholder="YouTubeChannel.com"
+                        labelPlacement="outside"
+                        value={channelUrl[index]}
+                        onChange={(e) => {
+                          const newChannelUrl = [...channelUrl];
+                          newChannelUrl[index] = e.target.value;
+                          setChannelUrl(newChannelUrl);
+                        }}
+                        startContent={
+                          <div className="pointer-events-none flex items-center">
+                            <span className="text-default-400 text-medium">
+                              https://
+                            </span>
+                          </div>
+                        }
+                      />
+                    </div>
+                  );
+                })}
+                <button
+                  onClick={() => {
+                    if (
+                      channelUrl.some((url) => url === "") ||
+                      channelUrl.length !== numChannels
+                    )
+                      return;
+                    subscribe();
+                  }}
+                  className={`text-lg f p-2 ${
                     channelUrl.some((url) => url === "") ||
                     channelUrl.length !== numChannels
-                  )
-                    return;
-                  subscribe();
-                }}
-                className={`text-lg f p-2 ${
-                  channelUrl.some((url) => url === "") ||
-                  channelUrl.length !== numChannels
-                    ? "bg-gray-200 text-black"
-                    : "bg-primary hover:bg-primary-600 transition-all animate-pulse ont-bold"
-                }  rounded-md  w-full`}
-              >
-                Subscribe
-              </button>
-            </div>
+                      ? "bg-gray-200 text-black"
+                      : "bg-primary hover:bg-primary-600 transition-all animate-pulse ont-bold"
+                  }  rounded-md  w-full`}
+                >
+                  Subscribe
+                </button>
+              </div>
+            </>
           )}
         </div>
       </CardBody>
