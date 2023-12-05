5648e90fdd991a953c3276ac315dfecff182594b
michaelperl <mperl0914@gmail.com>
2023-12-04 19:36:15 -0600

    minor

--- pages/subscriptions/index.tsx
+++ pages/subscriptions/index.tsx
@@ -78,20 +78,14 @@ export default function Subscriptions() {
                 </div>
               )}
             </div>
-
-            {stripeStatus.channels &&
-              Array.isArray(stripeStatus.channels) &&
-              stripeStatus.channels.length > 0 && (
-                <div className="flex flex-col items-start justify-start mt-8 gap-4 w-[80%]">
-                  <span className="font-bold text-lg">Your Channels:</span>
-
-                  <ul className="list-disc list-inside">
-                    {stripeStatus.channels.map((channel: string) => {
-                      return <li key={channel}>{channel}</li>;
-                    })}
-                  </ul>
-                </div>
-              )}
+            <div className="flex flex-col items-start justify-start mt-8 gap-4 w-[80%]">
+              <span className="font-bold text-lg">Your Channels:</span>
+              <ul className="list-disc list-inside">
+                {stripeStatus?.channels.map((channel: string) => {
+                  return <li key={channel}>{channel}</li>;
+                })}
+              </ul>
+            </div>
 
             <div className="flex flex-col items-start justify-start mt-8 gap-4 w-[80%]">
               <span className="font-bold text-lg">Your Downloads:</span>
